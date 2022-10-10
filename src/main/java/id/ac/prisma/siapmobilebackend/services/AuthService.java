f7dd603fbbdd56d7a6828245d4370b8197b46b74
Nofrets Poai <opetstudio@gmail.com>
2022-10-10 10:26:10 +0800

    edit koneksi mysql

--- src/main/java/id/ac/prisma/siapmobilebackend/services/AuthService.java
+++ src/main/java/id/ac/prisma/siapmobilebackend/services/AuthService.java
@@ -1,4 +1,12 @@
 package id.ac.prisma.siapmobilebackend.services;
 
 public class AuthService {
+    public boolean emailAndPasswordValidation(String email, String password) {
+        // fetch data from database
+
+        // compare with email and password
+        // if valid return true
+        // if not valid return false
+        return true;
+    }
 }
